bd2208fc795827214180044dd4d8b670c4ad2c58
picot244 <solal.picot@etu.unicaen.fr>
2025-09-16 21:46:23 +0200

    [FEATURE] création des méthodes draw de Plateforme et Balle

--- javascript/balle.js
+++ javascript/balle.js
@@ -1,5 +1,14 @@
+import Forme from "./forme.js";
+
 class Balle extends Forme {
     constructor(x, y) {
         super(x, y);
+    }
+
+    draw(ctx) {
+        ctx.fillstyle = "black";
+        ctx.beginPath();
+        ctx.arc(x, y, 50, 0, 2 * Math.PI);
+        ctx.stroke();
     }
 }
--- javascript/plateforme.js
+++ javascript/plateforme.js
@@ -1,5 +1,12 @@
+import Forme from "./forme.js";
+
 class Plateforme extends Forme {
     constructor(x, y) {
         super(x, y);
+    }
+
+    draw(ctx) {
+        ctx.fillstyle("black");
+        ctx.fillRect(x, y, 10, 50);
     }
 }
