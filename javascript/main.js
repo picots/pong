34232e98a7af02411981fa191dd3e4b8203ebcd7
picot244 <solal.picot@etu.unicaen.fr>
2025-09-30 16:14:32 +0200

    [MERGE] fusion avec la branche algo_pong

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,96 @@
+class Form {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Ball extends Form {
+    constructor(x, y, r, speed) {
+        super(x, y)
+        this.r = r;
+        this. vx = speed;
+        this.vy = speed;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "white";
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "blue";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+        ctx.lineWidth = 1;
+    }
+
+    accelerate(){
+        if(timer % 10 == 0 && timer != 0 && this.vx < 3*speed && this.vy < 3*speed){
+            this.vy *= 1 + Math.random()/1000;
+            this.vx *= 1 + Math.random()/1000;
+        }
+    }
+
+    update(canvas, n, platform) {
+        this.x += this.vx * n;
+        this.y -= this.vy;
+        this.accelerate();
+        if (this.x + this.r > canvas.width || this.x - this.r < 0)
+            this.vx *= -1;
+        if (this.y - this.r < 0)
+            this.vy *= -1;
+        if (this.y + this.r > canvas.height)
+            isEnd = true;
+        if (this.y + this.r >= platform.y && this.x >= platform.x && this.x <= platform.x + 70) {
+            this.vy *= -1;
+            this.vx += Math.random()/10;
+        }
+    }
+}
+
+class Platform extends Form {
+    constructor(x, y, speed) {
+        super(x, y);
+        this.speed = speed;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "blue";
+        ctx.beginPath();
+        ctx.roundRect(this.x, this.y, 70, 10, 5);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    update(canvas, keys) {
+        if (isMobileDevice()) {
+            if (keys["right"] && this.x + 70 < canvas.width) this.x += this.speed;
+            else if (keys["left"] && this.x > 0) this.x -= this.speed;
+        }
+        else {
+            if (keys["ArrowRight"] && this.x + 70 < canvas.width) this.x += this.speed;
+            else if (keys["ArrowLeft"] && this.x > 0) this.x -= this.speed;
+        }
+    }
+}
+
+const speed = 5;
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
-const gauche = document.getElementById("gauche");
-const droite = document.getElementById("droite");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const newGame = document.getElementById("newGame");
+const score = document.getElementById("score");
+
+let ball;
+let platform;
+
+let rafId;
+let isEnd;
+let n;
+let timer;
+let keys = {};
+let timeInterval;
 
 function isMobileDevice() {
     if (navigator.userAgent.match(/iPhone/i)
@@ -17,19 +106,59 @@ function isMobileDevice() {
 }
 
 function resize() {
-    if(isMobileDevice()){
-        gauche.hidden = droite.hidden = false;
+    if (isMobileDevice()) {
+        left.hidden = right.hidden = false;
         canvas.height = 500;
     }
     else
-        gauche.hidden = droite.hidden = true;
-        
+        left.hidden = right.hidden = true;
 }
 
 function draw() {
-    ctx.fillstyle = "black";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    ball.draw(ctx);
+    platform.draw(ctx);
 }
 
-resize();
-draw();
+function loop() {
+    if (!isEnd) {
+        ball.update(canvas, n, platform);
+        platform.update(canvas, keys);
+        draw();
+        score.innerHTML = "Score : "+ timer + "s";
+        rafId = requestAnimationFrame(loop);
+    }
+    else
+        alert("Vous avez perdu !");
+}
+
+function start() {
+    if(timeInterval)
+        clearInterval(timeInterval);
+    if (rafId) {
+        cancelAnimationFrame(rafId);
+        rafId = null;
+    }
+    if (isMobileDevice()) {
+        left.addEventListener("touchstart", () => keys["left"] = true);
+        left.addEventListener("touchend", () => keys["left"] = false);
+        right.addEventListener("touchstart", () => keys["right"] = true);
+        right.addEventListener("touchend", () => keys["right"] = false);
+    }
+    else {
+        document.addEventListener("keydown", e => keys[e.key] = true);
+        document.addEventListener("keyup", e => keys[e.key] = false);
+    }
+    resize();
+    timer = 0;
+    timeInterval = setInterval(()=>timer++, 1000);
+    ball = new Ball(canvas.width / 2, canvas.height - 17, 7, speed);;
+    platform = new Platform(canvas.width / 2 - 35, canvas.height - 10, speed);
+    isEnd = false;
+    n = Math.random() < 0.5 ? -1 : 1;
+    draw();
+    loop();
+}
+
+newGame.addEventListener("click", () => start());
